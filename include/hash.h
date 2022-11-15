099d959cae0431cc79fecc5516042d9fe48ebeb0
minxinhao <550279741@qq.com>
2022-11-15 02:26:59 +0000

    .gitignore is now working

--- include/hash.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include <stddef.h>
-
-size_t hash_1(const void* _ptr, size_t _len);
-size_t hash_2(const void* _ptr, size_t _len);
-__uint128_t hash(const void *key, const int len, const uint32_t seed = 0) noexcept;
-
-
