a9fac1e06a29bade62f2c61217f2039fdbe396f5
minxinhao <550279741@qq.com>
2022-11-23 13:49:34 +0000

    Edit run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,20 @@ do
     sshpass -p 'mxh' scp ./ser_cli mxh@$cli:/home/mxh/
     sshpass -p 'mxh' scp ../ser_cli.sh mxh@$cli:/home/mxh/
 done
-# echo "mxh" | sudo -S ../ser_cli.sh 0 1>out.txt & sshpass -p 'mxh' ssh mxh@192.168.1.88 " echo "mxh" | sudo -S ~/ser_cli.sh 1 1>out.txt "
+
+cnt=1
+for cli in ${clis[@]}
+do 
+    echo $cnt
+    sshpass -p 'mxh' ssh mxh@$cli " echo "mxh" | sudo -S ~/ser_cli.sh $cnt 1>out_$cnt.txt " &
+    ((cnt += 1))
+done
+echo "mxh" | sudo -S ../ser_cli.sh 0 1>out.txt
+
+cnt=1
+for cli in ${clis[@]}
+do 
+    echo "cli" $cli
+    sshpass -p 'mxh' scp mxh@$cli:/home/mxh/out_$cnt.txt .
+    ((cnt += 1))
+done
