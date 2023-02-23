df3fde474b2dddd2428dc8d701f60c1e3e6147c4
minxinhao <550279741@mail.com>
2023-02-23 12:00:32 +0000

    Edit Run.sh/Sync.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,13 @@
 #       server: sudo ../ser_cli.sh server
 #       client_0: sudo ../ser_cli.sh 0
 #       client_1: sudo ../ser_cli.sh 1
-git checkout .
-git pull
-cd build && make
+
+# git checkout .
+# git pull
+# cd build && make
+
+# clis=("192.168.1.33" "192.168.1.44" "192.168.1.51" "192.168.1.52" "192.168.1.53" "192.168.1.88" "192.168.1.69")
+
 # cnt=1
 # for cli in ${clis[@]}
 # do 
@@ -14,3 +18,11 @@ cd build && make
 #     ((cnt += 1))
 # done
 # echo "mxh" | sudo -S ../ser_cli.sh 0 1>out.txt
+
+
+if [ $1 = 0 ]
+then
+    ../ser_cli.sh 0 1>out0.txt
+else
+    ./ser_cli.sh $1 1>out$1.txt
+fi
--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 # usage: 
 #       ./sync.sh out/in
-clis=("192.168.1.33" "192.168.1.44" "192.168.1.51" "192.168.1.52" "192.168.1.53" "192.168.1.88" "192.168.1.69")
+# clis=("192.168.1.33" "192.168.1.44" "192.168.1.51" "192.168.1.52" "192.168.1.53" "192.168.1.88" "192.168.1.69")
+clis=("192.168.1.88" "192.168.1.33" "192.168.1.51")
 if [ "$1" = "out" ]
 then
     make
@@ -9,6 +10,7 @@ then
         echo "cli" $cli
         sshpass -p 'mxh' scp ./ser_cli mxh@$cli:/home/mxh/
         sshpass -p 'mxh' scp ../ser_cli.sh mxh@$cli:/home/mxh/
+        sshpass -p 'mxh' scp ../run.sh mxh@$cli:/home/mxh/
     done
 else
     cnt=1
