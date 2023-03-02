991375d114e9babc0c6aadaee0bec76344e8435c
minxinhao <550279741@mail.com>
2023-03-02 06:16:35 +0000

    Add some log && use pure_write instead of CAS in splithash_2b for fp bitmap

--- test/ser_cli.cc
+++ test/ser_cli.cc
@@ -16,9 +16,9 @@
 #define ORDERED_INSERT
 Config config;
 uint64_t load_num = 10000000;
-using ClientType = SPLIT_OP2B::Client;
-using ServerType = SPLIT_OP2B::Server;
-using Slice = SPLIT_OP2B::Slice;
+using ClientType = RACE::Client;
+using ServerType = RACE::Server;
+using Slice = RACE::Slice;
 
 inline uint64_t GenKey(uint64_t key)
 {
